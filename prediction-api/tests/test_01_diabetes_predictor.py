04d2d777138a486f4f860e9856790014499cbb0e
IndikaKuma <indika.kuma@gmail.com>
2023-03-12 16:52:22 +0100

    Fix path in test cases

--- prediction-api/tests/test_01_diabetes_predictor.py
+++ prediction-api/tests/test_01_diabetes_predictor.py
@@ -12,11 +12,11 @@ class TestDiabetesPredictor:
 
     @pytest.fixture(scope="session", autouse=True)
     def execute_before_any_test(self):
-        os.environ["MODEL_NAME"] = "../testResources/model.pkl"
+        os.environ["MODEL_NAME"] = "testResources/model.pkl"
 
     # your setup code goes here, executed ahead of first test
     def test_predict_single_record(self):
-        with open('../testResources/prediction_request.json') as json_file:
+        with open('testResources/prediction_request.json') as json_file:
             data = json.load(json_file)
         dp = diabetes_predictor.DiabetesPredictor()
         dp.load_model()
